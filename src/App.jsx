4338144cccf5eb7f1beb67d1f36152b9e88d272c
FabTrindade <fabrtrindade@gmail.com>
2022-06-12 19:38:21 -0300

    Adding 404 Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './assets/css/base/base.css'
 
 import Home from './paginas/Home'
+import Pagina404 from './paginas/Pagina404'
 import Sobre from './paginas/Sobre'
 
 function App() {
 
   return (
     <Router>
-      <switch>
+      <Switch>
         <Route exact path='/'>
           <Home />
         </Route>
         <Route path='/sobre'>
           <Sobre />
         </Route>
-      </switch>
+        <Route>
+          <Pagina404 />
+        </Route>
+      </Switch>
     </Router>
   )
 }
