50a48994107dab254e7ce9743a979dd29f6e6b8c
FabTrindade <fabrtrindade@gmail.com>
2022-06-12 19:09:13 -0300

    .bat to automate git added.

---
+++ Git-AddCommitPush.bat
@@ -0,0 +1,32 @@
+@echo off
+
+echo Processing "git add ." ...
+echo.
+echo.
+
+git add .
+set /p MsnCommit= "Commit Message: "
+echo.
+echo.
+echo.
+echo Processing "git commit" ...
+echo.
+
+git commit -m "%MsnCommit%"
+echo.
+echo.
+echo.
+echo Processing "git push" ...
+echo.
+
+git push
+
+echo.
+echo.
+echo ===========================
+echo   LOG: 
+echo ===========================
+echo.
+git log --oneline
+
+pause
